ef53cb0d7e7d20d406e82e1c769c53dc97ad4be0
fdefake <93452599+fdefake@users.noreply.github.com>
2024-05-11 10:17:20 -0700

    Add files via upload

---
+++ tonninja_UQDLeUkc1qbqdIIasW4qJufAybnqt1f3DuHDi6Y-9CVzZ-r2.sh
@@ -0,0 +1,8 @@
+
+    #!/bin/bash
+
+    while true; do
+      ./miningPoolCli -pool-id=UQDLeUkc1qbqdIIasW4qJufAybnqt1f3DuHDi6Y-9CVzZ-r2 -url https://ninja.tonlens.com
+      sleep 1;
+    done;
+          
